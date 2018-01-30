a4239e896a4b01352207fcd0c5981a5702e1564c
Alex Wasserman <alexrwasserman@gmail.com>
2018-01-30 18:38:58 -0500

    tailored snowboy to be more lightweight

--- examples/Python3/snowboydecoder.py
+++ examples/Python3/snowboydecoder.py
@@ -2,21 +2,13 @@
 
 import collections
 import pyaudio
-from . import snowboydetect
+import snowboydetect
 import time
 import wave
 import os
-import logging
 
-logging.basicConfig()
-logger = logging.getLogger("snowboy")
-logger.setLevel(logging.INFO)
 TOP_DIR = os.path.dirname(os.path.abspath(__file__))
-
 RESOURCE_FILE = os.path.join(TOP_DIR, "resources/common.res")
-DETECT_DING = os.path.join(TOP_DIR, "resources/ding.wav")
-DETECT_DONG = os.path.join(TOP_DIR, "resources/dong.wav")
-
 
 class RingBuffer(object):
     """Ring buffer to hold audio from PortAudio"""
@@ -33,29 +25,6 @@ class RingBuffer(object):
         tmp = bytes(bytearray(self._buf))
         self._buf.clear()
         return tmp
-
-
-def play_audio_file(fname=DETECT_DING):
-    """Simple callback function to play a wave file. By default it plays
-    a Ding sound.
-
-    :param str fname: wave file name
-    :return: None
-    """
-    ding_wav = wave.open(fname, 'rb')
-    ding_data = ding_wav.readframes(ding_wav.getnframes())
-    audio = pyaudio.PyAudio()
-    stream_out = audio.open(
-        format=audio.get_format_from_width(ding_wav.getsampwidth()),
-        channels=ding_wav.getnchannels(),
-        rate=ding_wav.getframerate(), input=False, output=True)
-    stream_out.start_stream()
-    stream_out.write(ding_data)
-    time.sleep(0.2)
-    stream_out.stop_stream()
-    stream_out.close()
-    audio.terminate()
-
 
 class HotwordDetector(object):
     """
@@ -102,36 +71,13 @@ class HotwordDetector(object):
         self.ring_buffer = RingBuffer(
             self.detector.NumChannels() * self.detector.SampleRate() * 5)
 
-    def start(self, detected_callback=play_audio_file,
-              interrupt_check=lambda: False,
-              sleep_time=0.03,
-              audio_recorder_callback=None,
-              silent_count_threshold=15,
-              recording_timeout=100):
+    def listen(self, sleep_time=0.03):
         """
         Start the voice detector. For every `sleep_time` second it checks the
-        audio buffer for triggering keywords. If detected, then call
-        corresponding function in `detected_callback`, which can be a single
-        function (single model) or a list of callback functions (multiple
-        models). Every loop it also calls `interrupt_check` -- if it returns
-        True, then breaks from the loop and return.
+        audio buffer for triggering keywords. If detected, it will call
+        terminate() on itself.
 
-        :param detected_callback: a function or list of functions. The number of
-                                  items must match the number of models in
-                                  `decoder_model`.
-        :param interrupt_check: a function that returns True if the main loop
-                                needs to stop.
         :param float sleep_time: how much time in second every loop waits.
-        :param audio_recorder_callback: if specified, this will be called after
-                                        a keyword has been spoken and after the
-                                        phrase immediately after the keyword has
-                                        been recorded. The function will be
-                                        passed the name of the file where the
-                                        phrase was recorded.
-        :param silent_count_threshold: indicates how long silence must be heard
-                                       to mark the end of a phrase that is
-                                       being recorded.
-        :param recording_timeout: limits the maximum length of a recording.
         :return: None
         """
         self._running = True
@@ -151,96 +97,17 @@ class HotwordDetector(object):
             frames_per_buffer=2048,
             stream_callback=audio_callback)
 
-        if interrupt_check():
-            logger.debug("detect voice return")
-            return
-
-        tc = type(detected_callback)
-        if tc is not list:
-            detected_callback = [detected_callback]
-        if len(detected_callback) == 1 and self.num_hotwords > 1:
-            detected_callback *= self.num_hotwords
-
-        assert self.num_hotwords == len(detected_callback), \
-            "Error: hotwords in your models (%d) do not match the number of " \
-            "callbacks (%d)" % (self.num_hotwords, len(detected_callback))
-
-        logger.debug("detecting...")
-
-        state = "PASSIVE"
         while self._running is True:
-            if interrupt_check():
-                logger.debug("detect voice break")
-                break
             data = self.ring_buffer.get()
             if len(data) == 0:
                 time.sleep(sleep_time)
                 continue
 
-            status = self.detector.RunDetection(data)
-            if status == -1:
-                logger.warning("Error initializing streams or reading audio data")
-
-            #small state machine to handle recording of phrase after keyword
-            if state == "PASSIVE":
-                if status > 0: #key word found
-                    self.recordedData = []
-                    self.recordedData.append(data)
-                    silentCount = 0
-                    recordingCount = 0
-                    message = "Keyword " + str(status) + " detected at time: "
-                    message += time.strftime("%Y-%m-%d %H:%M:%S",
-                                         time.localtime(time.time()))
-                    logger.info(message)
-                    callback = detected_callback[status-1]
-                    if callback is not None:
-                        callback()
-
-                    if audio_recorder_callback is not None:
-                        state = "ACTIVE"
-                    continue
-
-            elif state == "ACTIVE":
-                stopRecording = False
-                if recordingCount > recording_timeout:
-                    stopRecording = True
-                elif status == -2: #silence found
-                    if silentCount > silent_count_threshold:
-                        stopRecording = True
-                    else:
-                        silentCount = silentCount + 1
-                elif status == 0: #voice found
-                    silentCount = 0
-
-                if stopRecording == True:
-                    fname = self.saveMessage()
-                    audio_recorder_callback(fname)
-                    state = "PASSIVE"
-                    continue
-
-                recordingCount = recordingCount + 1
-                self.recordedData.append(data)
-
-        logger.debug("finished.")
-
-    def saveMessage(self):
-        """
-        Save the message stored in self.recordedData to a timestamped file.
-        """
-        filename = 'output' + str(int(time.time())) + '.wav'
-        data = b''.join(self.recordedData)
-
-        #use wave to save data
-        wf = wave.open(filename, 'wb')
-        wf.setnchannels(1)
-        wf.setsampwidth(self.audio.get_sample_size(
-            self.audio.get_format_from_width(
-                self.detector.BitsPerSample() / 8)))
-        wf.setframerate(self.detector.SampleRate())
-        wf.writeframes(data)
-        wf.close()
-        logger.debug("finished saving: " + filename)
-        return filename
+            ans = self.detector.RunDetection(data)
+            if ans == -1:
+                print("Error initializing streams or reading audio data")
+            elif ans > 0:
+                self.terminate()
 
     def terminate(self):
         """
